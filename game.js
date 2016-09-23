42f86dd5a0c4800e6c92194bef3521b6539bf41a
ninja25538 <ninja_william@hotmail.com>
2016-09-23 12:37:21 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,8 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "black";
-ctx.fillRect(0, 20, 10, 10);
+//Draws his "face"
+ctx.fillRect(0, 20, 5, 5);
+ctx.fillRect(0, 25, 5, 5);
+ctx.fillRect(5, 25, 5, 5);
+ctx.fillRect(5, 20, 5, 5);
