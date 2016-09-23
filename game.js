4bcb44031286545eb2b4f6c660e80853b4472939
ninja25538 <ninja_william@hotmail.com>
2016-09-23 11:44:24 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
-var c=document.getElementById("myCanvas");
-var ctx=c.getContext("2d");
-ctx.fillRect(300,20,150,100);
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, 80, 80);
