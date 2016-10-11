c4ba125f407ec47863c1e284fb41fb8b51fcc467
ninja25538 <ninja_william@hotmail.com>
2016-10-11 11:36:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var ctx=c.getContext("2d");
   ctx.fillStyle = "#0B0B0B";
   ctx.canvas.width = window.outerWidth;
   ctx.canvas.height = window.outerHeight;
+  
 //player's location
 var playerX = outerWidth / 2;
 var playerY = outerHeight / 2;
@@ -18,9 +19,11 @@ var inFight = false;
 //Whether the Protagonist can move or not
 var movement = true;
 
+
 //bandit's location
 var banditX = outerWidth / 2;
 var banditY = outerHeight / 2;
+
 
   //Draws the main character
   function drawProtagonistFacingRight(){
@@ -68,9 +71,96 @@ ctx.fillStyle = "#0B0B0B";
 ctx.fillRect(playerX, playerY + 15, 15, 25);
   }
   
+  
   //Draws the Bandit
+  function drawBanditFacingRight(){
+     //His face
+    ctx.fillStyle = "#FFDE7A";
+    ctx.fillRect(banditX + 2, banditY, 20, 25);
+    
+    //His shirt
+    ctx.fillStyle = "black";
+    ctx.fillRect(banditX + 5, banditY + 25, 8, 25);
+    
+    ctx.fillStyle = "#FFDE7A";
+    //His arm
+    ctx.fillRect(banditX + 5, banditY + 24, 8, 28);
+    
+    //His eye
+    ctx.fillStyle = "black";
+    ctx.fillRect(banditX + 14, banditY + 5, 5, 8);
+    //His bandit thing on his mouth? I don't know what it's called
+    ctx.fillStyle = "red";
+    ctx.fillRect(banditX + 4, banditY + 15, 18, 10);
+    
+    //His pant leg
+    ctx.fillStyle = "#11226B";
+    ctx.fillRect(banditX + 5, banditY + 49, 8, 20);
 
+    //his shoe
+    ctx.fillStyle = "black";
+    ctx.fillRect(banditX + 5, banditY + 68, 7.5, 5);
+  }
+  
   function drawBanditFacingLeft(){
+    //His face
+    ctx.fillStyle = "#FFDE7A";
+    ctx.fillRect(banditX - 6, banditY, 20, 25);
+    
+    //His shirt
+    ctx.fillStyle = "black";
+    ctx.fillRect(banditX + 2, banditY + 25, 8, 25);
+    
+    ctx.fillStyle = "#FFDE7A";
+    //His arm
+    ctx.fillRect(banditX + 2, banditY + 24, 8, 28);
+    
+    //His eye
+    ctx.fillStyle = "black";
+    ctx.fillRect(banditX - 4, banditY + 5, 5, 8);
+    //His bandit thing on his mouth? I don't know what it's called
+    ctx.fillStyle = "red";
+    ctx.fillRect(banditX - 7, banditY + 15, 18, 10);
+    
+    //His pant leg
+    ctx.fillStyle = "#11226B";
+    ctx.fillRect(banditX + 2, banditY + 49, 8, 20);
+
+    //His shoe
+    ctx.fillStyle = "black";
+    ctx.fillRect(banditX + 2, banditY + 68, 7.5, 5);
+  }
+  
+  function drawBanditFacingUp(){
+     //His face
+    ctx.fillStyle = "#FFDE7A";
+    ctx.fillRect(banditX, banditY, 25, 25);
+    
+    //His shirt
+    ctx.fillStyle = "black";
+    ctx.fillRect(banditX + 5, banditY + 25, 16, 25);
+    
+    //His arms
+    //His right arm (YOUR RIGHT!!!!)
+    ctx.fillStyle = "#FFDE7A";
+    ctx.fillRect(banditX + 21, banditY + 25, 8, 20);
+    //His left arm (On your left side)
+    ctx.fillRect(banditX - 3, banditY + 25, 8, 20);
+    
+    //His pants
+    //His right pant leg (ur right)
+    ctx.fillStyle = "#11226B";
+    ctx.fillRect(banditX + 12, banditY + 49, 8, 20);
+    ctx.fillRect(banditX + 5, banditY + 49, 8, 20);
+    ctx.fillStyle = "black";
+    ctx.fillRect(banditX + 11, banditY + 53, 3, 15);
+
+    //his shoes
+    ctx.fillStyle = "black";
+    ctx.fillRect(banditX + 5, banditY + 68, 15, 5);
+  }
+  
+  function drawBanditFacingDown(){
         //His face
     ctx.fillStyle = "#FFDE7A";
     ctx.fillRect(banditX, banditY, 25, 25);
@@ -104,31 +194,35 @@ ctx.fillRect(playerX, playerY + 15, 15, 25);
 
     //his shoes
     ctx.fillStyle = "black";
-    ctx.fillRect(banditX, banditY + 68, 25, 10);
+    ctx.fillRect(banditX + 5, banditY + 68, 15, 5);
   }
-    
-drawProtagonistFacingRight();
- movement = true;
+  
+  //Draws all map scenes
+  
+  
+drawBanditFacingLeft();
+ movement = false;
+ 
 //Adds movement to game.
 document.addEventListener('keydown', function(e){
-   if(e.key === 'w' && playerY >= 5 && inFight === false && movement === true){
+   if(e.key === 'w' && playerY >= 5 && movement === true){
    playerY-= playerSpeed;
    ctx.clearRect(0, 0, outerWidth, outerHeight);
   drawProtagonistFacingUp();
    }
-  if(e.key == 'd' && playerX < outerWidth - 50 && inFight === false && movement === true){
+  if(e.key == 'd' && playerX < outerWidth - 50 && movement === true){
     playerX+= playerSpeed;
    ctx.clearRect(0,0,outerWidth, outerHeight);
   drawProtagonistFacingRight();
   }
   
-  if(e.key == 's' && playerY < outerHeight - 100 && inFight === false && movement === true){
+  if(e.key == 's' && playerY < outerHeight - 100 && movement === true){
     playerY+= playerSpeed;
    ctx.clearRect(0,0,outerWidth, outerHeight);
   drawProtagonistFacingDown();
   }
   
-  if(e.key == 'a' && playerX > 5 && inFight === false && movement === true){
+  if(e.key == 'a' && playerX > 5 && movement === true){
     playerX-= playerSpeed;
    ctx.clearRect(0, 0,outerWidth, outerHeight);
   drawProtagonistFacingLeft();
@@ -140,4 +234,3 @@ document.addEventListener('keydown', function(e){
 
 
 };
-
