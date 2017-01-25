b22c14cd1f742fc0228d1686c8c773f230d735f9
ninja25538 <ninja_william@hotmail.com>
2017-01-25 13:09:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -260,7 +260,7 @@ ctx.fillRect(playerX + 7.5, playerY + 5, 10, 12.5);
 	  
 	  ctx.fillStyle = "#FFDE7A";
 	  //His arm
-    ctx.fillRect(banditX + 6, banditY + 24, 25, 7);
+    ctx.fillRect(banditX, banditY + 24, 25, 7);
     
     
     //His eye
